09f0ebff9a34fe3cd052ce90b85748d6524c7b93
nilspeterson15 <nils@ksu.edu>
2019-02-07 15:39:16 -0600

    Add TestConnection functionality using the token request

--- src/main/java/ksu/rgn/arcgis/jobs/TestConnectionJ.java
+++ src/main/java/ksu/rgn/arcgis/jobs/TestConnectionJ.java
@@ -6,33 +6,28 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ksu.rgn.utils.Future;
 
 /**
  *
  */
 public class TestConnectionJ extends GISJob {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TestConnectionJ.class);
+    private final String clientId, clientSecret;
+
+    public TestConnectionJ(String clientId, String clientSecret){
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
 
     @Override
     public void run() {
-        try {
-
-            final JSONObject r = request("/ArcGIS/rest/info/healthCheck", null);
-            if ((boolean) r.get("success")) {
-                future.invokeSuccess("Server is running");
-            } else {
-                future.invokeFail("Could not contact server");
-            }
-
-        } catch (WebbException we) {
-            LOG.warn("GIS Server could not be reached");
-            future.invokeFail("The server could not be reached");
-        } catch (JSONException e) {
-            LOG.warn("GIS Server replied with unexpected response");
-            LOG.debug("Cause: {}", e);
-            future.invokeFail("Unexpected response");
-        }
+        Future f = bridge.addJob(new RequestTmpTokenJ(clientId, clientSecret));
+        f.onSuccess(o -> {
+            future.invokeSuccess("Successful connection to the ArcGIS database");
+        });
+        f.onFail(o -> {
+            future.invokeFail("Failed to connect to the ArcGIS database");
+        });
     }
-
 }
