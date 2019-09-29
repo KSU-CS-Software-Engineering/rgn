f21bdd1205feca743abe2f985ccb3efeac862aa6
colin1224 <colin.cope@gmail.com>
2019-09-29 01:23:41 -0500

    Final dependency fixes.

--- GroceryImplementation/src/test/java/com/CIS642/Rural/GroceryImplementation/GroceryImplementationApplicationTests.java
+++ GroceryImplementation/src/test/java/com/CIS642/Rural/GroceryImplementation/GroceryImplementationApplicationTests.java
@@ -1,5 +1,7 @@
 package com.CIS642.Rural.GroceryImplementation;
 
+import com.CIS642.Rural.GroceryImplementation.gui.Window;
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
