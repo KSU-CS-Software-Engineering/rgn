fba80959fc15ae04777fcc4e9ac17142afcfbd54
alihamadah <alihamadah@k-state.edu>
2019-11-13 11:07:37 -0600

    small restyle

--- RuralGroceryBlazor/TruckState.cs
+++ RuralGroceryBlazor/TruckState.cs
@@ -10,9 +10,9 @@ namespace RuralGroceryBlazor
         [Required, MaxLength(100)] public string Name { get; set; }
         [Required, MaxLength(100)] public string Capacity { get; set; }
         [Required, MaxLength(100)] public double Start_Location_x { get; set; }
-        public bool Refrigerated;
-        public bool Start;
-        public bool End;
+        public bool Refrigerated { get; set; }
+        public bool Start { get; set; }
+        public bool End { get; set; }
         [Required, MaxLength(100)] public double Start_Location_y { get; set; }
         [Required, MaxLength(100)] public double End_Location_x { get; set; }
         [Required, MaxLength(100)] public double End_Location_y { get; set; }
