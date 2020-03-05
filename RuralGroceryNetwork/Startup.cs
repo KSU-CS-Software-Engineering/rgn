d5dc13a2e1310588d33259583bc86ac7280696e3
colin1224 <colin.cope@gmail.com>
2020-03-05 02:54:20 -0600

    Change environmental variable access

--- RuralGroceryNetwork/Startup.cs
+++ RuralGroceryNetwork/Startup.cs
@@ -43,10 +43,10 @@ namespace RuralGroceryNetwork
 #if RELEASE
 			services.AddDbContext<RuralGroceryDB.Data.RuralGrocery.RuralGroceryContext>(options =>
 			options.UseSqlServer(
-				Configuration.GetConnectionString("SQLAZURECONNSTR_DefaultConnection")));
+				Configuration.GetConnectionString(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_DefaultConnection"))));
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(
-					Configuration.GetConnectionString("SQLAZURECONNSTR_DefaultConnection")));
+					Configuration.GetConnectionString(Environment.GetEnvironmentVariable("SQLAZURECONNSTR_DefaultConnection"))));
 #endif
 
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
