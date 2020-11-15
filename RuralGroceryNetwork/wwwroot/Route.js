00a2542f1a11c304f9438666ebf546d61ad0d6e5
Matt Llewelyn <mjllewelyn@gmail.com>
2020-11-15 16:50:05 -0600

    Added being able to change the basemap

--- RuralGroceryNetwork/wwwroot/Route.js
+++ RuralGroceryNetwork/wwwroot/Route.js
@@ -3,8 +3,9 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/widgets/Directions"
-], function (Map, MapView, Directions) {
+    "esri/widgets/Directions",
+    "esri/Graphic"
+], function (Map, MapView, Directions, Graphic) {
     var map = new Map({
         basemap: "streets-navigation-vector"
     });
@@ -21,4 +22,62 @@ require([
         routeServiceUrl: "https://utility.arcgis.com/usrsvcs/appservices/6g6tiL0fLOmFllkm/rest/services/World/Route/NAServer/Route_World"
     });
     view.ui.add(directions, "top-right");
+
+    function ChangeMapBase() {
+
+        var x = document.getElementById("MapBase").value;
+        console.log(x);
+        map.basemap = x;
+
+    }
+    window.ChangeMapBase = ChangeMapBase;
+
+    //Add nodes on the map by a given input
+    function MapInput(lat, lon) {
+
+        var Point = {
+            type: "point",
+            longitude: lon,
+            latitude: lat,
+        };
+        if (view.graphics.length === 0) {
+            console.log(view.graphics.length);
+            addGraphic("start", Point);
+
+
+        } else if (view.graphics.length === 1) {
+            console.log(view.graphics.length);
+            addGraphic("finish", Point);
+            // Call the route service
+            //getRoute();
+        } else {
+            console.log(view.graphics.length);
+            view.graphics.removeAll();
+            addGraphic("start", Point);
+        }
+        console.log("MapFromInput lon: " + lon + " lat: " + lat);
+    }
+
+    window.MapInput = MapInput;
+
+    function addGraphic(type, point) {
+        var graphic = new Graphic({
+            symbol: {
+                type: "simple-marker",
+                color: (type === "start") ? "green" : "red",
+                size: "10px"
+            },
+            geometry: point
+
+        });
+        view.graphics.add(graphic);
+    }
+
+    function centerMap(lat, lon) {
+
+        view.center = [lon, lat];
+        console.log("Centered the Map:  lon:" + lon + " lat:" + lat);
+
+    }
+    window.centerMap = centerMap;
 });
