19909f8498ff80c740987361e1f9b87b64e82633
Caroline Snell <Caroline Snell@ksu.edu>
2021-04-22 15:46:56 -0500

    Added Images to help page, Sort of formated images

--- RuralGroceryNetwork/wwwroot/Route.js
+++ RuralGroceryNetwork/wwwroot/Route.js
@@ -1,12 +1,9 @@
 /*
 Copyright 2020 Kansas State University
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -83,8 +80,6 @@ require([
         view.on("click", function (event) {
             /*if (view.graphics.length === 0) {
                 addGraphicClick("start", event.mapPoint);
-
-
             } else if (view.graphics.length === 1) {
                 addGraphicClick("end", event.mapPoint);
                 // Call the route service
