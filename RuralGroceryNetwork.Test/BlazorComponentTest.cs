3a121712e7bdeb9eba625ea0f4c140dd3c106347
Carlos Cerna <cbcerna@ksu.edu>
2021-04-23 21:12:00 -0500

    Update BlazorComponentTest.cs

--- RuralGroceryNetwork.Test/BlazorComponentTest.cs
+++ RuralGroceryNetwork.Test/BlazorComponentTest.cs
@@ -16,7 +16,6 @@ limitations under the License.
 
 using Bunit;
 using System;
-using Bunit.Mocking.JSInterop;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using RuralGroceryNetwork.Pages;
@@ -25,28 +24,65 @@ using Index = RuralGroceryNetwork.Pages.Index;
 
 namespace RuralGroceryNetwork.Test
 {
-	public class BlazorComponentTest : ComponentTestFixture
+	public class BlazorComponentTest
 	{
 		[Fact]
 		public void IndexComponentTest()
 		{
-			IRenderedComponent<Index> cut = RenderComponent<Index>();
 
-			var expectedHtml = @"<h1>Rural Grocery Initiative</h1>
-								<p>
-								  <br>The mission of the
-								  <a href=""https://www.ruralgrocery.org/"">Rural Grocery Initiative</a>
-								(RGI) is to provide resources to help sustain and enhance independently-owned rural grocery stores.  RGI assists
-									communities and citizens to strengthen rural grocery operations and improve access to healthy foods.
-								</p>
-
-								<p>
-								This application is meant to aid rural grocers by optimizing their distribution networks. It takes in information about stores or other distribution points
-									of interest and will calculate the best way to load trucks, the order to distribute to stores in, etc. The application gives the user control to manipulate
-									nodes (a store or distribution center), trucks, demand, and more. To accomplish the routing portion of the calculations, the application utilizes ArcGIS
-									Online, a service that can find the best routes between nodes on a map.								
-								</p>";
-			cut.MarkupMatches(expectedHtml);
 		}
-	}
+
+        [Fact]
+        public void Login() { 
+
+        }
+        [Fact]
+        public void Logout() {
+        
+        }
+        [Fact]
+        public void RegisterAccount() {
+        
+        }
+        [Fact]
+        public void NavigateToRoutes() {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<RouteEditor>();
+            var paraElm = cut.Find("row1");
+
+            //// Act
+            //cut.Find("button").Click();
+            //var paraElmText = paraElm.TextContent;
+
+            //// Assert
+            //paraElmText.MarkupMatches("Current count: 1");
+
+            Assert.True(paraElm.ClassName == "row1");
+
+        }
+        
+        [Fact]
+        public void RendersAboutPage() {
+        
+        }
+
+        [Fact]
+        public void RendersDistributionSim() {
+        
+        }
+
+        [Fact]
+        public void RendersRGIPage() {
+        
+        }
+
+        [Fact]
+        public void RendersInstructionsPage() {
+        
+        
+        }
+
+
+    }
 }
